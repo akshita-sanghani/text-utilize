9859bdfe4d907469706ab00113a7648f5b9d1cad
akshita sanghani <akshitasanghani8866@gmail.com>
2025-02-27 12:46:22 +0530

    Updated files

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -6,8 +6,12 @@ def index(request):
     return render(request, 'index.html',params)
 
 def analyze(request):
-    dtext = request.GET.get("text", "default")
-    removepunc = request.GET.get('removepunc', 'off')
+    dtext = request.POST.get("text", "default")
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    extraspaceremover = request.POST.get('extraspaceremover', 'off')
+    charcount = request.POST.get('charcount', 'off')
     if removepunc == 'on':
         punctuations = '''...()-[]{};:'"\,<>./?!@#$%^&*_~'''
         analyzed = ''
@@ -18,6 +22,48 @@ def analyze(request):
             'purpose': 'Remove Punctuations',
             'analyzed_text' : analyzed
         }
-        return render(request, 'analyze.html', params)
-    else:
-        return HttpResponse("Error")
+        dtext = analyzed
+
+    if (fullcaps == "on"):
+        analyzed = ''
+        for char in dtext:
+                analyzed = analyzed + char.upper()
+        params = {
+            'purpose': 'Change To Uppercase',
+            'analyzed_text': analyzed
+        }
+        dtext = analyzed
+
+    if (newlineremover == "on"):
+        analyzed = ''
+        for char in dtext:
+            if char != '\n' and char != "\r":
+                analyzed = analyzed + char
+        params = {
+            'purpose': 'New Line Remover',
+            'analyzed_text': analyzed
+        }
+        dtext = analyzed
+
+    if (extraspaceremover == "on"):
+        analyzed = ''
+        for index, char in enumerate(dtext):
+            if not (dtext[index] == ' ' and dtext[index + 1] == ' '):
+                analyzed = analyzed + char
+        params = {
+            'purpose': 'Extra Space Remover',
+            'analyzed_text': analyzed
+        }
+        dtext = analyzed
+
+    if (charcount == "on"):
+        analyzed = len(dtext)
+        params = {
+            'purpose': 'Character Count',
+            'analyzed_text': analyzed
+        }
+
+    if (removepunc != 'on' and fullcaps != "on" and newlineremover != "on" and extraspaceremover != "on" and charcount != "on"):
+        return HttpResponse("Please select the opration")
+
+    return render(request, 'analyze.html', params)
